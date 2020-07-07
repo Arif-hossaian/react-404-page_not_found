2ef5963c93884fc5132c4aa23d17b00ff5b52ea4
arifhossain123 <arifhossain42136@gmail.com>
2020-07-07 10:17:36 +0600

    Updates

---
+++ precache-manifest.4abbc6bcf1666660c0cb8213b11082cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4ad309498376d86e5131a1af9b9afc",
+    "url": "/react-404-page_not_found/index.html"
+  },
+  {
+    "revision": "1d3864d7e8e7cf060eb1",
+    "url": "/react-404-page_not_found/static/css/main.0901db4a.chunk.css"
+  },
+  {
+    "revision": "bb2930c0ab3b1e25c7a5",
+    "url": "/react-404-page_not_found/static/js/2.f783d0dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-404-page_not_found/static/js/2.f783d0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3864d7e8e7cf060eb1",
+    "url": "/react-404-page_not_found/static/js/main.8b334bc0.chunk.js"
+  },
+  {
+    "revision": "f9848bc085df7bf5d30e",
+    "url": "/react-404-page_not_found/static/js/runtime-main.4a0f6fa0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-404-page_not_found/static/media/logo.5d5d9eef.svg"
+  }
+]);
